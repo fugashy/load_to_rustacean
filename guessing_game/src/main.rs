f75c34841669dabf3fd77d8f9347ee601036863a
fugashy <fugashy@icloud.com>
2020-03-05 20:38:29 +0900

    Improve game system
    - Use loop for trying more time
    - Deprecate printing of secret number
    - Use match to handle the error and validate result

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -11,34 +11,42 @@ use std::io;
 fn main() {
     println!("Guess the number!");
 
-    let secret_number = rand::thread_rng().gen_range(1, 101);
-    println!("The secret number is: {}", secret_number);
+    loop {
+        println!("Please input your guess.");
 
-    println!("Please input your guess.");
+        // mut予約後で変更可能な変数を宣言する
+        // 空のStringオブジェクトに束縛されている可変変数を作っている
+        let mut guess = String::new();
 
-    // mut予約後で変更可能な変数を宣言する
-    // 空のStringオブジェクトに束縛されている可変変数を作っている
-    let mut guess = String::new();
+        // .read_lineではResultを返す
+        // それはハンドリングされなければならない
+        // .expect("xxx")でOkかErrかを判定しないといけない
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
 
-    // .read_lineではResultを返す
-    // それはハンドリングされなければならない
-    // .expect("xxx")でOkかErrかを判定しないといけない
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
+        // shadowingしてもとの値を覆い隠す
+        // guess_strみたいなカッコ悪いことはしなくてもよい！
+        // trim()は\nを削除してくれる
+        // parse()はなんらかの数値にしてくれる
+        // match式はエラー処理の一般的な方法
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            // _は包括値（どんなものでも構わないという意思表示
+            Err(_) => continue,
+        };
 
-    // shadowingしてもとの値を覆い隠す
-    // guess_strみたいなカッコ悪いことはしなくてもよい！
-    // trim()は\nを削除してくれる
-    // parse()はなんらかの数値にしてくれる
-    let guess: u32 = guess.trim().parse().expect("Please type a number");
+        // {}はpythonでもおなじみなプレースホルダー
+        println!("You guessed: {}", guess);
 
-    // {}はpythonでもおなじみなプレースホルダー
-    println!("You guessed: {}", guess);
-
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("Too small!"),
-        Ordering::Greater => println!("Too big!"),
-        Ordering::Equal => println!("You win!"),
+        let secret_number = rand::thread_rng().gen_range(1, 101);
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
     }
 }

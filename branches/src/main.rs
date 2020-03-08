159c0f6061583e13e12102e3be22a2e714c1dbe2
fugashy <fugashy@icloud.com>
2020-03-08 15:32:16 +0900

    Add sample of loop statement

--- branches/src/main.rs
+++ branches/src/main.rs
@@ -1,3 +1,50 @@
+fn if_statement() {
+    let number = 3;
+
+    if number < 5 {
+        println!("condition was true");
+    } else {
+        println!("condition was false");
+    }
+}
+
+fn use_if_statement_in_let_statement() {
+    let condition = true;
+    let number = if condition {
+        // 値として評価されたいのでセミコロンなし
+        5
+    } else {
+        6
+    };
+
+    println!("the value of number is: {}", number);
+}
+
+fn loop_statement() {
+    let mut count = 0;
+    loop {
+        count += 1;
+        if count == 10 {
+            break;
+        }
+        println!("again");
+    }
+    println!("out of loop");
+}
+
+fn while_statement() {
+    let mut number = 3;
+
+    while number != 0 {
+        println!("{}", number);
+        number -= 1;
+    }
+    println!("out of while");
+}
+
 fn main() {
-    println!("Hello, world!");
+    if_statement();
+    use_if_statement_in_let_statement();
+    loop_statement();
+    while_statement();
 }

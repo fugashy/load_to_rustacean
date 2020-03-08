72187e7ae2ca2a774c27d4e70d6f46ba472ed584
fugashy <fugashy@icloud.com>
2020-03-08 13:41:53 +0900

    Extract updation and shodowing as function

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,7 +1,26 @@
-fn main() {
+fn update() {
     // あとでいじりたいのでmutableにする
     let mut x = 5;
     println!("The value of x is: {}", x);
     x = 6;
     println!("The value of x is: {}", x);
+}
+
+fn shodowing() {
+    let x = 5;
+    // 6
+    let x = x + 1;
+    // 12
+    let x = x * 2;
+    println!("x is: {}", x);
+
+    // 文字列型から数値型のシャドウイングも許される
+    let spaces = "    ";
+    let spaces = spaces.len();
+    println!("length of spaces is :{}", spaces);
+}
+
+fn main() {
+    update();
+    shodowing();
 }

d45703c3c6da9e02e6ea1aaf172caf328996fd4b
fugashy <fugashy@icloud.com>
2020-03-08 15:38:55 +0900

    Add samples of for statement

--- branches/src/main.rs
+++ branches/src/main.rs
@@ -42,9 +42,35 @@ fn while_statement() {
     println!("out of while");
 }
 
+fn scan_list_by_while_statement() {
+    let a = [10, 20, 30, 40, 50];
+    let mut index = 0;
+    while index < 5 {
+        println!("the value is {}:", a[index]);
+        index += 1;
+    }
+}
+
+fn for_statement() {
+    let a = [10, 20, 30, 40, 50];
+
+    for element in a.iter() {
+        println!("the value is {}:", element);
+    }
+}
+
+fn scan_range_type() {
+    for number in 1..4 {
+        println!("the number is {}:", number);
+    }
+}
+
 fn main() {
     if_statement();
     use_if_statement_in_let_statement();
     loop_statement();
     while_statement();
+    scan_list_by_while_statement();
+    for_statement();
+    scan_range_type();
 }

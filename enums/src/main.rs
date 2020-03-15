ac0bcd1edd070f8c9a4a6ef5206b7822bb596e3b
fugashy <fugashy@icloud.com>
2020-03-15 15:16:59 +0900

    Add simple sample of enum

--- enums/src/main.rs
+++ enums/src/main.rs
@@ -1,3 +1,22 @@
+// std::fmt::Displayが定義されてないのでDebugで表示しながらすすめる
+#[derive(Debug)]
+enum IpAddrKind {
+    V4(String),
+    V6(String),
+}
+
+fn print(ip_type: IpAddrKind) {
+    println!("type of ip is: {:?}", ip_type);
+}
+
+fn simple_usage() {
+    let home = IpAddrKind::V4(String::from("127.0.0.1"));
+    let loopback = IpAddrKind::V6(String::from("::1"));
+
+    print(home);
+    print(loopback);
+}
+
 fn main() {
-    println!("Hello, world!");
+    simple_usage();
 }

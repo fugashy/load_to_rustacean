3dae04305ee43655d4a359f09a140012586d5385
fugashy <fugashy@icloud.com>
2020-03-15 15:27:47 +0900

    Add simples sample of enum

--- enums/src/main.rs
+++ enums/src/main.rs
@@ -2,6 +2,7 @@
 #[derive(Debug)]
 enum IpAddrKind {
     // 異なる型の定義ができる!
+    // 値・文字・構造体なんでもあり
     V4(u8, u8, u8, u8),
     V6(String),
 }
@@ -18,6 +19,32 @@ fn simple_usage() {
     print(loopback);
 }
 
+// 使っていない属性をコンパイラに無視してもらう
+// この記法は直後の定義についてのみ効果を発揮する
+#[allow(dead_code)]
+enum Message {
+    // データなし
+    Quit,
+    // 匿名構造体
+    Move { x: i32, y: i32 },
+    // String
+    Write(String),
+    // 3つの値をもつ
+    ChangeColor(i32, i32, i32),
+}
+
+impl Message {
+    fn call(&self) {
+        return;
+    }
+}
+
+fn use_message() {
+    let m = Message::Write(String::from("hello"));
+    m.call();
+}
+
 fn main() {
     simple_usage();
+    use_message();
 }

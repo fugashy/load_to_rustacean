e29977973f8d4a25222bb356324ac526096cc822
fugashy <fugashy@icloud.com>
2020-04-10 21:23:00 +0900

    Arrange modules

--- communicator/src/lib.rs
+++ communicator/src/lib.rs
@@ -1,13 +1,14 @@
 // コンパイラは標準でlib.rsのみを検索する
 // 以下のモジュールを定義しておけばそのモジュール名に対応するファイルを作って行けば良くなる
 // そういうふうにできている
-mod client;
-mod network;
+pub mod client;
+pub mod network;
 
 #[cfg(test)]
 mod tests {
+    use super::client;
     #[test]
     fn it_works() {
-        assert_eq!(2 + 2, 4);
+        client::connect();
     }
 }

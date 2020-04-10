bb57c177af16ebd7c0f6ef757d8431c0f4a3091c
fugashy <fugashy@icloud.com>
2020-04-10 21:29:46 +0900

    Add tests

--- communicator/src/lib.rs
+++ communicator/src/lib.rs
@@ -1,14 +1,42 @@
 // コンパイラは標準でlib.rsのみを検索する
 // 以下のモジュールを定義しておけばそのモジュール名に対応するファイルを作って行けば良くなる
 // そういうふうにできている
+//
+// communicator
+//   - pub client
+//      pub connect()
+//   - pub network
+//      pub connect()
+//      - pub server
+//          - pub connect()
+//
+//   - tests
+//
+// lib.rs
+// client.rs
+// network
+//  - mod.rs
+//  - server.rs
 pub mod client;
 pub mod network;
 
 #[cfg(test)]
 mod tests {
     use super::client;
+    use super::network;
+
     #[test]
-    fn it_works() {
+    fn try_to_connect_by_client() {
         client::connect();
+    }
+
+    #[test]
+    fn try_to_connect_by_network() {
+        network::connect();
+    }
+
+    #[test]
+    fn try_to_connect_by_server() {
+        network::server::connect();
     }
 }

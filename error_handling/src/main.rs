35fc7ff632a74e55a360d5667b030756401cc83d
fugashy <fugashy@icloud.com>
2020-04-19 21:09:00 +0900

    Add a sample of match-guard

--- error_handling/src/main.rs
+++ error_handling/src/main.rs
@@ -1,3 +1,4 @@
+use std::fs::File;
 /*
  * Rustには2種類のエラー分類がある
  * - 回復可能
@@ -21,7 +22,27 @@ fn try_to_buffer_overread() {
     println!("v[{}] is: {}", 99, v[99]);
 }
 
+fn try_to_read_file_then_handling() {
+    let filename = String::from("/tmp/hello.txt");
+    let _f = File::open(&filename);
+
+    let _f = match _f {
+        Ok(file) => file,
+        // 見つからない場合は作ってみる
+        // if ...はマッチガードと呼ばれる
+        // TODO(fugashy) &ではなくrefを使用する理由は後ほど
+        Err(ref error) if error.kind() == std::io::ErrorKind::NotFound => {
+            match File::create(&filename) {
+                Ok(fc) => fc,
+                Err(e) => panic!("Tried to create a file but there was a problem: {:?}", e),
+            }
+        }
+        Err(error) => panic!("There was a problem opening the file: {:?}", error),
+    };
+}
+
 fn main() {
     // simple_panic();
     // try_to_buffer_overread();
+    try_to_read_file_then_handling();
 }

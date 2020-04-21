f62a5198c5a13614a51123e97b804f33b0fd5e4b
fugashy <fugashy@icloud.com>
2020-04-21 22:43:09 +0900

    Implement a function that search largest value in list

--- generics/src/main.rs
+++ generics/src/main.rs
@@ -1,3 +1,21 @@
+fn largest_i32(list: &[i32]) -> i32 {
+    let mut largest = list[0];
+
+    for &number in list.iter() {
+        if number > largest {
+            largest = number;
+        }
+    }
+    largest
+}
+
+fn test_largest_i32() {
+    let number_list = vec![34, 50, 25, 100, 65];
+    println!("The largest number is: {}", largest_i32(&number_list));
+    let number_list = vec![102, 34, 6000, 89, 65];
+    println!("The largest number is: {}", largest_i32(&number_list));
+}
+
 fn main() {
-    println!("Hello, world!");
+    test_largest_i32();
 }

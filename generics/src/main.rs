e30cb898764a5f7d24a3427dcf8503b6cbe04531
fugashy <fugashy@icloud.com>
2020-04-22 18:49:17 +0900

    Add a generic function that search largest of T

--- generics/src/main.rs
+++ generics/src/main.rs
@@ -21,6 +21,19 @@ fn largest_char(list: &[char]) -> char {
     largest
 }
 
+// 比較とコピーセマンティックを使用できる型のみに制限する
+// コンパイラが教えてくれて，Tに求められることが明示されるのがいいな
+fn largest<T: std::cmp::PartialOrd + Copy>(list: &[T]) -> T {
+    let mut largest = list[0];
+
+    for &item in list.iter() {
+        if item > largest {
+            largest = item;
+        }
+    }
+    largest
+}
+
 fn test_largest_i32() {
     let number_list = vec![34, 50, 25, 100, 65];
     println!("The largest number is: {}", largest_i32(&number_list));
@@ -33,7 +46,15 @@ fn test_largest_char() {
     println!("The largest char is: {}", largest_char(&char_list));
 }
 
+fn test_generic_largest() {
+    let number_list = vec![34, 50, 25, 100, 65];
+    println!("The largest number is: {}", largest(&number_list));
+    let char_list = vec!['y', 'm', 'a', 'q'];
+    println!("The largest char is: {}", largest(&char_list));
+}
+
 fn main() {
     test_largest_i32();
     test_largest_char();
+    test_generic_largest();
 }

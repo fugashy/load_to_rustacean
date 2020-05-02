3f11b49578b5d2244db09179a37512c44f62ebde
fugashy <fugashy@icloud.com>
2020-05-03 01:31:07 +0900

    Add tests of Rectangle

--- adder/src/lib.rs
+++ adder/src/lib.rs
@@ -2,6 +2,19 @@
 //
 // また，ドキュメントに現れたコード例もコンパイルされる
 // すごいな
+
+// 以前作ったStructのテストをしてみる
+pub struct Rectangle {
+    length: u32,
+    width: u32,
+}
+
+impl Rectangle {
+    pub fn can_hold(&self, other: &Rectangle) -> bool {
+        self.length > other.length && self.width > other.width
+    }
+}
+
 #[cfg(test)]
 mod tests {
     // テスト関数であることを示唆する
@@ -14,5 +27,33 @@ mod tests {
     #[test]
     fn another() {
         panic!("Make this test fail");
+    }
+
+    use super::Rectangle;
+
+    #[test]
+    fn larger_can_hold_smaller() {
+        let larger = Rectangle {
+            length: 8,
+            width: 7,
+        };
+        let smaller = Rectangle {
+            length: 5,
+            width: 1,
+        };
+        assert!(larger.can_hold(&smaller));
+    }
+
+    #[test]
+    fn smaller_cannot_hold_larger() {
+        let larger = Rectangle {
+            length: 8,
+            width: 7,
+        };
+        let smaller = Rectangle {
+            length: 5,
+            width: 1,
+        };
+        assert!(!smaller.can_hold(&larger));
     }
 }

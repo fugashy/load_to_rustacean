34e84e15b93ceabf722532a0a571d0d221eb2919
fugashy <fugashy@icloud.com>
2020-05-03 15:49:14 +0900

    Add a commented assertion

--- adder/src/lib.rs
+++ adder/src/lib.rs
@@ -21,6 +21,10 @@ pub fn add_two(a: i32) -> i32 {
     a + 2
 }
 
+pub fn greeting(name: &str) -> String {
+    format!("hello {}", name)
+}
+
 #[cfg(test)]
 mod tests {
     // テスト関数であることを示唆する
@@ -68,5 +72,17 @@ mod tests {
     #[test]
     fn it_add_two() {
         assert_eq!(4, add_two(2));
+    }
+
+    use super::greeting;
+
+    #[test]
+    fn greeting_contains_name() {
+        let result = greeting("Carol");
+        assert!(
+            result.contains("Carol"),
+            "Greeting did not contain name, value was '{}'",
+            result
+        );
     }
 }

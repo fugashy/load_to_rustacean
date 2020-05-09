7b2e98404d183835346cd41e8b963637a5db2d0f
fugashy <fugashy@icloud.com>
2020-05-09 17:19:27 +0900

    Modify to use stderr

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -9,13 +9,14 @@ fn main() {
     let args: Vec<String> = env::args().collect();
     // unwrap_or_elseはErrの時にユーザ定義クロージャをコールしてくれる
     let c = Config::new(&args).unwrap_or_else(|err| {
-        println!("Problem parsing arguments: {}", err);
+        // 標準エラーストリームに出力
+        eprintln!("Problem parsing arguments: {}", err);
         process::exit(1);
     });
 
     // runがErrを返したかどうか
     if let Err(e) = run(c) {
-        println!("Application error: {}", e);
+        eprintln!("Application error: {}", e);
         process::exit(1);
     }
 }

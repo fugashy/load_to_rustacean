4788a1ad5235ece2c59a5359f1477d9b54b6b9ab
fugashy <fugashy@icloud.com>
2020-05-09 15:30:48 +0900

    Modify to show contents in specified file

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,4 +1,7 @@
 use std::env;
+use std::fs::File;
+// 入出力処理に有用なトレイトを含んでいる
+use std::io::prelude::*;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -8,4 +11,12 @@ fn main() {
 
     println!("Searching for: {}", query);
     println!("In file: {}", filename);
+
+    let mut f = File::open(filename).expect("Failed to find");
+
+    let mut contents = String::new();
+    f.read_to_string(&mut contents)
+        .expect("Something went wrong reading the file");
+
+    println!("With text:\n{}", contents);
 }

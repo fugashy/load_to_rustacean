2039be20fe49de64e4894a870673d00a8a6fa694
fugashy <fugashy@icloud.com>
2020-05-09 14:54:50 +0900

    Improve unit test

--- adder/src/lib.rs
+++ adder/src/lib.rs
@@ -2,6 +2,9 @@
 //
 // また，ドキュメントに現れたコード例もコンパイルされる
 // すごいな
+//
+// 関数名を指定すると，その関数のみ実行される
+// 指定した文字と合致するテストが実行される．これもすごい
 
 // pubにしないと内部関数となる
 // この場合，使われないとdead_codeワーニングがでる
@@ -13,10 +16,31 @@ pub fn greeting(name: &str) -> String {
     format!("hello {}", name)
 }
 
+// 関数の出力はテストライブラリによってキャプチャされる(非表示)
+// それは制御可能
+pub fn prints_and_returns_10(a: i32) -> i32 {
+    // cargo testのデフォルトは，この出力をキャプチャする
+    // 表示したい場合は-- --nocaptureオプションを付加する
+    println!("I got the value: {}", a);
+    10
+}
+
+// 非公開
+fn internal() -> String {
+    String::from("I am internal test")
+}
+
+pub fn avoid_dead_code() {
+    println!("{}", internal());
+}
+
+// 単体テストをするときはcfg(test)を使うことが慣例になっている
 #[cfg(test)]
 mod tests {
     // テスト関数であることを示唆する
     #[test]
+    // 無視することもできる
+    // #[ignore]
     fn exploration() {
         // アサーションマクロ
         assert_eq!(2 + 2, 4);
@@ -44,5 +68,27 @@ mod tests {
             "Greeting did not contain name, value was '{}'",
             result
         );
+    }
+
+    use super::prints_and_returns_10;
+
+    #[test]
+    fn this_test_will_pass() {
+        let value = prints_and_returns_10(0);
+        assert_eq!(value, 10);
+    }
+
+    #[test]
+    fn this_test_will_fail() {
+        let value = prints_and_returns_10(0);
+        assert_eq!(value, 5);
+    }
+
+    use super::internal;
+
+    // もちろん非公開関数のテストも単体テストの範疇
+    #[test]
+    fn validate_internal() {
+        assert_eq!(internal(), "I am internal test");
     }
 }

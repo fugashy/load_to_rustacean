289cd4be2ba441c1fe49765f7e34de76499edc77
fugashy <fugashy@icloud.com>
2020-05-11 21:25:58 +0900

    Modify to call only once

--- functional/src/lib.rs
+++ functional/src/lib.rs
@@ -9,23 +9,15 @@ pub mod closures {
     }
 
     fn generate_workout_without_closure(intensity: u32, random_number: u32) {
+        let expensive_result = simulated_expensive_calculation(intensity);
         if intensity > 25 {
-            println!(
-                "Today, do {} pushups",
-                simulated_expensive_calculation(intensity)
-            );
-            println!(
-                "Next, do {} situps",
-                simulated_expensive_calculation(intensity)
-            );
+            println!("Today, do {} pushups", expensive_result);
+            println!("Next, do {} situps", expensive_result);
         } else {
             if random_number == 3 {
                 println!("Take a break today! Remember to stray hydrated");
             } else {
-                println!(
-                    "Today, run for {} minites",
-                    simulated_expensive_calculation(intensity)
-                );
+                println!("Today, run for {} minites", expensive_result);
             }
         }
     }
